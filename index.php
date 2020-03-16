60805c2aca7565e20c5df2c722915c5a19084d87
root <root@localhost.localdomain>
2020-03-16 21:59:47 +0700

    update

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $api_url = $is_production ? 'https://app.midtrans.com/snap/v1/transactions' :
                             'https://app.sandbox.midtrans.com/snap/v1/transactions';
             if (!strpos($_SERVER['REQUEST_URI'], '/charge')) {
                 http_response_code(404);
-                echo "Wrong path, make sure it's ' /charge'";
+                echo "Wrong path, make sure it's '/charge'";
                 exit();
             }
 
